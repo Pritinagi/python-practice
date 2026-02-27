ec8fdc5ca55bdeb331d7134943f8f1bac8d08256
Pritinagi <Pritinagig9494@gmail.com>
2026-02-27 18:00:25 +0530

    Add python files

---
+++ string/02_math.py
@@ -0,0 +1,19 @@
+# math
+password ='123a'
+print(len(password))
+
+
+if len(password)<3:
+    print(" password is short ")
+
+
+text ="""
+hey i am priti .
+i am learning python. 
+actually relearning.
+python is easy and powerfull.
+
+
+
+"""
+print(text.count("Python"))
