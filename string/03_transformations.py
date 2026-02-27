3a3ed20d093d1176e54908ddbc9603ecef762e0f
Pritinagi <Pritinagig9494@gmail.com>
2026-02-27 18:36:15 +0530

    ADD Python files

--- string/03_transformations.py
+++ string/03_transformations.py
@@ -12,5 +12,12 @@ print(pri.replace("$","").replace(",",""))
 first_name="Priti"
 last_name="Nagi"
 
-full_name=first_name+ " " +last_name
+full_name=first_name+ " - " +last_name
 print(full_name)
+  
+
+# used this in project 
+folder ="c:/users/priti/"
+file = "report.csv"
+full_file_path =folder + file
+print(full_file_path)
