3f98dabc9af3057918dc1f36c1cf32d94abad995
Pritinagi <Pritinagig9494@gmail.com>
2026-06-24 11:35:40 +0530

    added file to PYTHON Folders

---
+++ basic/while_loop.py
@@ -0,0 +1,25 @@
+# # i=0
+# # while i<4:
+# #     print(f"helllo {i} ")
+# #     i=i+1
+
+
+# # answer=""
+# # while answer !="yes":
+# #     answer=input("do you agree???")
+# # print("thank you ")
+
+
+# answer = ''
+# while answer !='stop':
+#     answer=input("you ana continue or stop")
+#     print("hie we are continuing again ")
+
+
+
+while True:
+    answer = input (" do you agreee?? yes or no ")
+    if answer == "yes":
+        break
+
+print("thnak you ")
---
+++ challenges/eleven.py
@@ -0,0 +1,12 @@
+i=1
+answer = ""
+while i<=3:
+        answer = input("do you agree?? yes/No : ")
+        i+=1
+        if answer=="yes":
+            print("glad we are on same page")
+
+            break
+        
+else:
+            print("3 strikes, you are out")
